8e20eecb9e176d26b2bfeaea712befabbb7ca466
marcoazzurrini <marco.azzurrini.art@gmail.com>
2019-09-26 14:06:34 +0200

    fix cors policy

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,24 @@
+// ASSIGN DOM ELEMENTS TO VARIABLES
+const humanInput = document.getElementById('human-input');
+const yodishOutput = document.getElementById('yoda-output');
 class Translation {
     constructor(text) {
         this.text = text;
     }
 
-    traslate() {
-        
+    translate() {     
+    fetch(`https://cors-anywhere.herokuapp.com/http://yoda-api.appspot.com/api/v1/yodish?text=${humanInput.value}`)
+        .then(res => res.json())
+        .then(data => {
+            yodishOutput.value = data.yodish;
+        })
+        .catch(err => console.log(err));
     }
 }
 
+// LIST FOR KEYUP HUMAN INPUT
+humanInput.addEventListener('keyup', () => {
+    const newTranwslate = new Translation(humanInput.value);
+    newTranwslate.translate();
+})
 
-fetch(`https://crossorigin.me/http://yoda-api.appspot.com/api/v1/yodish?text=hello`)
-            .then(res => res.json())
-            .then(data => console.log(data));
